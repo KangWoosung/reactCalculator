7bf5eb2c386dc7c0c1edade5d7aabee531e76b0e
Apollo <script0null@gmail.com>
2023-09-07 17:50:45 +0500

    file changes
    Big changes
    
    Co-Authored-By: Polar Bear <102044298+umidevopz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./styles.css";
 import Calculator from './components/Calculator.js'
 
 function App() {
   return (
   <div className="App">
-  <Calculator />
+
+    <div className="calculator">
+      {/* output */}
+      <div className="output">
+        <div className="last-operand"></div>
+        <div className="current-operand"></div>
+
+        {/* Grid area */}
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>DEL</button>
+      </div>
+    </div>
   </div>
    );
 }
