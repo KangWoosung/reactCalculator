b1fc4909980c7ebf9b7f28a66e01e3739312f3d5
umidevop <umidevop@sysXcall>
2023-09-08 13:48:48 +0500

    new update

--- README.md
+++ README.md
@@ -8,12 +8,10 @@
 
 ### `npm install`
 
-Install all dependencies to run application. For more visit [react.dev](https://react.dev/docs)
+Install all dependencies to run application.
 
 ### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
-
-import "./App.css";
+import { useState }   from 'react'
 import "./styles.css";
+import "./App.css";
+
 
 
 function App() {
-  return (
-  <>
 
+  const [value, setValue] = useState('')
+
+
+  const clear = () =>{
+    setValue(value.slice(0, -1))
+  }
+  const reset = () =>{
+    setValue('')
+  }
+
+  const total = () =>{
+    try{
+      let evaluation = Function('return ' + value)()
+      setValue((prevState) => prevState + '=' + evaluation.toString() + '')
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  function handleButtonClick(buttonValue){
+    setValue(value + buttonValue)
+  }
+  return (
+    <>
     <div className="calculator">
       {/* output */}
       <div className="output">
-          <div className="last-operand">1562</div>
-          <div className="current-operand">1562</div>
+        <div className="input">{value}</div>
+
       </div>
         {/* Grid area */}
-        <button className="span-two">AC</button>
-        <button>DEL</button>
-        <button>÷</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className='span-two'>=</button>
+        <button className="span-two reset" onClick={reset} title='RESET'>AC</button>
+        <button onClick={clear} title='DELETE' > DEL</button>
+        <button onClick={() => handleButtonClick('/')}>÷</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button className='span-two ' onClick={total} title='TOTAL'>=</button>
     </div>
   </>
    );
